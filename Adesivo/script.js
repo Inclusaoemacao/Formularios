f3af4cd3a37e0ebdf18eb4f2795da5f239e02bd1
Inclusaoemacao <secretaria.inclusaoemacao@gmail.com>
2025-02-11 00:07:47 -0300

    Update script.js

--- Adesivo/script.js
+++ Adesivo/script.js
@@ -62,7 +62,7 @@ document.getElementById("sendFile").addEventListener("click", function() {
     // ✅ Exibir tela de carregamento antes de enviar
     document.getElementById("loading-overlay").style.display = "block";
 
-    const scriptURL = "https://script.google.com/macros/s/AKfycbwv8-GHhzX4pgB5dys1P_kf360ZeMe72KNiiJuIsr1pcOKrjm9CZyElwOD_39LRcaZG/exec"; // Substitua pelo seu ID correto
+    const scriptURL = "https://script.google.com/macros/s/AKfycbx-MUrkKCfECipU22w7DhO2K0TeY1cE6StXRYGbGNcaxNRqP9GO-_o2A9KK41LdyG1QsA/exec"; // Substitua pelo seu ID correto
 
     const formData = new FormData();
     formData.append("n_pedido", document.getElementById("n_pedido").value);
